7f2dd62dc0e738068761f4c3bf38ceda11a9f23c
shushuInBuaa <susan940910@hotmail.com>
2019-01-29 11:46:31 +0800

    commit

---
+++ routes/login.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if(req.query.username==req.query.password)
+    res.send({result:"1"});
+  else
+    res.send({result:"0"});
+
+});
+
+module.exports = router;
