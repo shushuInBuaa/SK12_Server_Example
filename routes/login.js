cebe355b5ed0e94c340d62a0efa869598f96ea7c
shushuInBuaa <susan940910@hotmail.com>
2019-02-01 12:20:48 +0800

    new version

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if(req.query.username==req.query.password)
+  if(req.query.username===req.query.password)
     res.send({result:"1"});
   else
     res.send({result:"0"});
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req,res,next){
-    if(req.query.username==req.query.password)
+    if(req.body.username===req.body.password)
         res.send({result:"1"});
     else
         res.send({result:"0"});
